29ae3ec21088599a14ebe6f13a0791304d2076fa
elena-brd <128292991+elena-brd@users.noreply.github.com>
2023-05-03 08:55:14 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById('mySidenavbar').style.width = '300px';
+}
+
+function closeNav() {
+    document.getElementById('mySidenavbar').style.width = '0px';
+}
